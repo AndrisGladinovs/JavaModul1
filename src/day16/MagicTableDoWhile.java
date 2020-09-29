152b6ce3474e7c454a7f7e82c8e39b1c93d367dd
Andris Gladinovs <andrisgladinovs@andriss-air.speedport.ip>
2020-09-29 12:25:06 +0200

    LogicalTasks 18-19

--- src/day16/MagicTableDoWhile.java
+++ src/day16/MagicTableDoWhile.java
@@ -1,21 +1,20 @@
 package day16;
 
-public class MultiplicationTableWhile {
+public class MagicTableDoWhile {
     public static void main(String[] args) {
         createTable(10);
     }
 
     private static void createTable(int number) {
         int i = 1;
-        while (i <= number) {
+        do {
             int j = 1;
-            while (j <= number) {
+            do {
                 System.out.printf("%3d", i * j);
                 j++;
-            }
-            i++;
+            } while (j <= number);
             System.out.println();
-        }
-
+            i++;
+        } while (i <= number);
     }
 }
